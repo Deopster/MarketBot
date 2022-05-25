e034dc26a4da6b893faac8aac462e37b56f6b9a0
Колдун Ебучий <andrey11551611@gmail.com>
2022-05-25 03:36:03 +0300

    fix

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from discord_webhook import DiscordWebhook, DiscordEmbed
+import num_det
 from mss import mss
 import mss
 import time
@@ -61,16 +62,17 @@ def find_ellement():
         x = int((pt[0] * 2 + w) / 2)
         y = int((pt[1] * 2 + h) / 2)
         print("Found ", x, y)
-        temp=0
+        temp=1
         while y>60:
             partscreen(420, 80, 60+y-40, 740, 2)
             y-=110
             time.sleep(0.1)
-            if temp==0:
+            if temp==1:
                 d=read("new_say.png")
                 s = d.split('\n')
                 name=s[0]
                 print(name)
+            #Обработка
             im = Image.open("new_say.png")
             img = im.convert("RGB")
             pixdata = img.load()
@@ -80,13 +82,35 @@ def find_ellement():
                         pixdata[x, u] = (255, 255, 255)
                     else:
                         pixdata[x, u] = (0, 0, 0)
-            img.crop((270, 55, 390, 77)).save('f.jpg', quality=100)
-            time.sleep(1)
-            i = Image.open('f.jpg')
-            p = pytesseract.image_to_string(i, config=r'--psm 6 -c tessedit_char_whitelist=0123456789')
-            print(p)
-            time.sleep(10000)
-            mess=name+" "+p
+            # функция стоймости
+            img.crop((274, 55, 390, 75)).save('f.jpg', quality=100)
+            time.sleep(0.1)
+            'fin_num.jpg'
+            # im = Image.open('fin_num.jpg')
+            im = Image.open('f.jpg')
+            p=num_det.detect(im,1)
+            # обработка изображения колличества
+            img.crop((25, 47, 75, 62)).save('numb.jpg', quality=100)
+            img1 = Image.open('numb.jpg')
+            pixdata = img1.load()
+            gt=False
+            for x in range(img1.size[0]):
+                if gt is True:
+                    break
+                if pixdata[x, 1] != (255, 255, 255):
+                    for u in range(img1.size[1]):
+                        pixdata[x, u] = (255, 255, 255)
+                else:
+                    # for m in range(x+20):
+                    #     for t in range(img1.size[1]):
+                    #         pixdata[m, t] = (255, 255, 255)
+                            gt=True
+            img1.save('fin_num.jpg', quality=100)
+            # функция получения значения
+            img2 = Image.open('fin_num.jpg')
+            allo = num_det.detect(img2,2)
+            # Вывод в дискорд
+            mess=name+" цена:"+str(p)+" колличество "+str(allo)
             send(temp,mess)
             temp+=1
             print(s)
---
+++ num_det.py
@@ -0,0 +1,138 @@
+from PIL import Image, ImageChops, ImageEnhance
+import itertools
+# Number list
+def detect(f,mode):
+    numbers=[
+    [1,[0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1]],
+    [4,[0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0]],
+    [4,[0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0]],
+    [0,[0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0]],
+    [1,[0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1]],
+    [3,[0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1]],
+    [8,[0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1]],
+    [7,[1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0]],
+    [9,[0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1]],
+    [9,[0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1]],
+    [1,[0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1]],
+    [4,[0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0]],
+    [3,[0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0]],
+    [9,[0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0]],
+    [8,[0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0]],
+    [8,[0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0]],
+    [0,[0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1]],
+    [0,[0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1]],
+    [4,[0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0]],
+    ]
+
+    numbers2=[
+        [1,[0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1]],
+        [0,[0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1]],
+        [1,[0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1]],
+        [0,[0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1]],
+        [0,[0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1]]
+    ]
+
+    recognit=""
+    img = f.convert("RGB")
+    pixdata = img.load()
+    temp=[]
+    res=[]
+    nums=[]
+    gren=[]
+    found=False
+    innum=0
+    for x in range(img.size[0]):
+        if innum >img.size[1]-3:
+            right=x-1
+            found = False
+            nums.append([left,right])
+        innum=0
+        for y in range(2,img.size[1]):
+            if pixdata[x, y]!=(255, 255, 255):
+                if found is False:
+                    left=x
+                    found=True
+            else:
+                if found is True:
+                    innum+=1
+    found=False
+    for x in reversed(range(img.size[0])):
+        if found is  True:
+            break
+        for y in range(5,img.size[1]-4):
+            if pixdata[x, y]!=(255, 255, 255):
+                right=x
+                found=True
+    for i in nums:
+        x, y = i
+        print(i)
+        if y-x<4:
+            gren.append(i)
+    for i in gren:
+        nums.remove(i)
+
+    # for i in nums:
+    #     x,y=i
+    #     for l in range(x,y):
+    #         pixdata[l, 11] = (255, 53, 53)
+
+    # for i in nums:
+    #     x,y=i
+    #     for l in range(x,y):
+    #         pixdata[l, 11] = (255, 53, 53)
+
+
+    # for i in nums:
+    #     incrementy = img.size[1] / 4
+    #     onenum,secnum=i
+    #     incrementx = (secnum-onenum)/4
+    #     basicx = incrementx
+    #     for x in range(4):
+    #         basey=incrementy
+    #         for y in range(4):
+    #             pixdata[onenum + basicx-incrementx/2, 0+basey-incrementy/2] = (255, 53, 53)
+    #             basey+=incrementy
+    #             print(basey)
+    #         basicx+=incrementx
+    # img.show()
+    for i in nums:
+        index=0
+        incrementy = img.size[1] / 4
+        onenum,secnum=i
+        incrementx = (secnum-onenum)/4
+        basicx = incrementx
+        for x in range(4):
+            basey=incrementy
+            for y in range(4):
+                if pixdata[onenum + basicx-incrementx/2, 0+basey-incrementy/2] == (0, 0, 0):
+                    temp.append(1)
+                else:
+                    temp.append(0)
+                basey+=incrementy
+            basicx+=incrementx
+        index+=1
+        res.append(temp)
+        temp=[]
+    print(res)
+    if mode==1:
+        for i in res:
+            for n in numbers:
+                for s in n:
+                    if i == s:
+                        recognit+=str(n[0])
+    else:
+        for i in res:
+            for n in numbers2:
+                for s in n:
+                    if i == s:
+                        recognit+=str(n[0])
+
+    print(recognit)
+    return int(recognit)
+
+    # if __name__ == '__main__':
+    #     print("Выберите режим 0-обучение 1-распознавание")
+    #     mode=input()
+
+
+
